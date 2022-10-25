c840869f01d4aabe408c3644aeca857529658860
Josh Claxton <jrccode@outlook.com>
2022-10-25 16:45:59 -0500

    work and kaitlyn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Navi from './components/Navi'
 import About from './components/About'
 import Skills from './components/Skills'
+import Work from './components/Work'
 import Contact from './components/Contact'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -11,6 +12,7 @@ function App() {
       <Navi />
       <About />
       <Skills />
+      <Work />
       <Contact />
     </div>
   )
--- src/data.js
+++ src/data.js
@@ -6,3 +6,18 @@ export const skills = [
   'MongoDB',
   'Material Design',
 ]
+
+export const work = [
+  {
+    title: 'Kaitlyn King',
+    subtitle: 'Develop/Design',
+    description:
+      'Website for the wonderful Kaitlyn King, nice informational with good looks and functionality.',
+    image: '../images/kaitlyn.png',
+    link: 'https://jrccoding.github.io/KaitlynKing1.0/',
+  },
+  {},
+  {},
+  {},
+  {},
+]
