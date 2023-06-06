bbe209ceef8cf989557adb61d7f3d43c93b8b4da
Josh Claxton <jrccode@outlook.com>
2023-06-06 09:49:47 -0500

    Work page reworked, added apps

--- client/src/screens/WorkScreen.js
+++ client/src/screens/WorkScreen.js
@@ -2,7 +2,8 @@ import { Icon } from '@iconify/react'
 import { Col, Row } from 'react-bootstrap'
 import kk_ss from '../images/kk_ss.png'
 import lnc_ss from '../images/lnc_ss.png'
-import she_ss from '../images/she_ss.png'
+import poke_ss from '../images/poke_ss.png'
+import swapi_ss from '../images/swapi_ss.png'
 import '../styles/Work.css'
 
 export default function Projects() {
@@ -30,17 +31,6 @@ export default function Projects() {
             />{' '}
           </a>
         </Col>
-        <Col className='mb-10'>
-          <a
-            href='https://stonyhill.herokuapp.com/'
-            className='text-2xl no-underline text-white visited:text-white hover:text-green'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <h2 className='text-white mt-20 text-center'>Ecommerce</h2>
-            <img src={she_ss} alt='ecommerce' className='work_img' />{' '}
-          </a>
-        </Col>
         <Col>
           <a
             href='https://jrccoding.github.io/KaitlynKing1.0/'
@@ -48,8 +38,34 @@ export default function Projects() {
             target='_blank'
             rel='noreferrer'
           >
-            <h2 className='text-white mt-20 text-center'>Personal</h2>
+            <h2 className='text-white mt-20 text-center'>Personal Site</h2>
             <img src={kk_ss} alt='kaitlyn king' className='work_img' />
+          </a>
+        </Col>
+        <Col className='mb-10'>
+          <a
+            href='https://jrcpokeapp.surge.sh'
+            className='text-2xl no-underline text-white visited:text-white hover:text-green'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <h2 className='text-white mt-20 text-center'>PokeAPI App</h2>
+            <img
+              src={poke_ss}
+              alt='Pokemon App Josh Claxton'
+              className='work_img'
+            />{' '}
+          </a>
+        </Col>
+        <Col>
+          <a
+            href='https://jrcswapi.surge.sh/'
+            className='text-2xl no-underline text-white visited:text-white hover:text-green'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <h2 className='text-white mt-20 text-center'>Star Wars API App</h2>
+            <img src={swapi_ss} alt='Star Wars API App' className='work_img' />
           </a>
         </Col>
       </Row>
