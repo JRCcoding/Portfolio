c5850585e4182dece026a8742a508d7a69086a31
Josh Claxton <jrccode@outlook.com>
2023-07-01 12:26:55 -0500

    quick mobile fix for contact

--- client/src/components/Personal.js
+++ client/src/components/Personal.js
@@ -20,9 +20,7 @@ const Personal = () => {
             className='-mt-40 mb-40 mx-auto'
             id='about'
           ></img> */}
-          <p className='text-8xl text-center -mt-40 mb-20 text-[rgb(0,175,145)]'>
-            Web Developer
-          </p>
+          <p className='webdeveloper text-[rgb(0,175,145)]'>Web Developer</p>
           <p className='text-white text-2xl sm:col-2 mx-auto indent-10 mb-40 w-1/2'>
             I am a <pre className='inline'>software engineer</pre>, I love
             building beautifully functional websites and web applications. I
